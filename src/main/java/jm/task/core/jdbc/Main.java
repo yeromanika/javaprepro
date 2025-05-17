5e83990a39592c7211016fa087be74276c25fe7f
yeromanika <yeromanika@gmail.com>
2025-05-17 22:16:55 +0300

    работа с бд

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc;
 
-public class Main {
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 20);
+        userService.saveUser("Петр", "Петров", (byte) 25);
+        userService.saveUser("Сидор", "Сидоров", (byte) 30);
+        userService.saveUser("Алексей", "Алексеев", (byte) 35);
+
+        List<User> users = userService.getAllUsers();
+        users.forEach(System.out::println);
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+    }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,44 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private final UserDao userDao = new UserDaoJDBCImpl();
+
+    @Override
     public void createUsersTable() {
-
+        userDao.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
-
+        userDao.dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        userDao.saveUser(name, lastName, age);
+        System.out.printf("User с именем %s добавлен в базу данных\n", name);
     }
 
+    @Override
     public void removeUserById(long id) {
-
+        userDao.removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = userDao.getAllUsers();
+        users.forEach(System.out::println);
+        return users;
     }
 
+    @Override
     public void cleanUsersTable() {
-
+         userDao.cleanUsersTable();
     }
 }
